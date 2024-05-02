5218ebc409309af0cc2db76e61c12e75d790f9dc
TInnaV1 <167511819+TInnaV1@users.noreply.github.com>
2024-05-02 16:02:42 +0300

    Update tz1.sh

--- TZ1/tz1.sh
+++ TZ1/tz1.sh
@@ -4,9 +4,17 @@ input_dir="$1"
 output_dir="$2"
 count=1
 
-find "$input_dir" -type f | while read -r file; do
-	name=$(basename "$file")
-	new_name="${count}_${name}"
-	cp "$file" "$output_dir/$new_name"
-	count=$((count + 1))
-done
+if [[ "$(ls -A "$input_dir")" ]]; then
+    for file in "$input_dir"/*; do
+        if [[ -d "$file" && "$(ls -A "$file")" ]]; then
+            chmod +xr "$file"/*
+        fi
+    done
+
+    find "$input_dir" -type f | while read -r file; do
+        name=$(basename "$file")
+        new_name="${count}_${name}"
+        cp "$file" "$output_dir/$new_name"
+        count=$((count + 1))
+    done
+fi 
